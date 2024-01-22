b51a6c08d4f95581862193ee944b950795aa9a54
Dmitriy <dm.stebenev@gmail.com>
2024-01-22 14:22:37 +0200

    added solution to the task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir(srcDir, destDir) {
+  try {
+    await fs.mkdir(destDir, { recursive: true });
+
+    const files = await fs.readdir(srcDir);
+
+    for (const file of files) {
+      const srcFilePath = path.join(srcDir, file);
+      const destFilePath = path.join(destDir, file);
+
+        const stats = await fs.stat(srcFilePath);
+
+      if (stats.isDirectory()) {
+        await copyDir(srcFilePath, destFilePath);
+      } else {
+        await fs.copyFile(srcFilePath, destFilePath);
+      }
+    }
+    console.log('Directory successfully copied!');
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+copyDir('04-copy-directory/files', '04-copy-directory/files-copy');
