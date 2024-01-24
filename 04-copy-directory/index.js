b36d7d74ffa300e7e429ca319c85ba9289040d98
Dmitriy <dm.stebenev@gmail.com>
2024-01-24 08:36:07 +0200

    fix:task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,13 +5,24 @@ async function copyDir(srcDir, destDir) {
   try {
     await fs.mkdir(destDir, { recursive: true });
 
-    const files = await fs.readdir(srcDir);
+    const files = await fs.readdir(destDir);
 
     for (const file of files) {
       const srcFilePath = path.join(srcDir, file);
       const destFilePath = path.join(destDir, file);
 
-        const stats = await fs.stat(srcFilePath);
+      if (!(await fs.existsSync(srcFilePath))) {
+        await fs.unlink(destFilePath);
+      }
+    }
+
+    const srcFiles = await fs.readdir(srcDir);
+
+    for (const file of srcFiles) {
+      const srcFilePath = path.join(srcDir, file);
+      const destFilePath = path.join(destDir, file);
+
+      const stats = await fs.stat(srcFilePath);
 
       if (stats.isDirectory()) {
         await copyDir(srcFilePath, destFilePath);
@@ -19,10 +30,10 @@ async function copyDir(srcDir, destDir) {
         await fs.copyFile(srcFilePath, destFilePath);
       }
     }
+
     console.log('Directory successfully copied!');
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
 }
-
 copyDir('04-copy-directory/files', '04-copy-directory/files-copy');
